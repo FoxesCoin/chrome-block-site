0637293fcdb4bd013b0d24e86c10289266a87b93
FoxesCoin <work.developer.denis@gmail.com>
2022-06-30 17:03:59 +0300

    bugfix/tab-clicking-blink

--- options/scripts/tab.js
+++ options/scripts/tab.js
@@ -3,6 +3,9 @@ const [weekly, daily] = Array.from(document.querySelectorAll(".tab__item"));
 const ACTIVE_TAB = "tab__item_active";
 const ACTIVE_FORM = "form_active";
 
+const WEEK_TAB = { tab: weekly, form: weeklyForm };
+const DAY_TAB = { tab: daily, form: dailyForm };
+
 const toggleClass = (tab) => {
 	tab.tab.classList.toggle(ACTIVE_TAB);
 	tab.form.classList.toggle(ACTIVE_FORM);
@@ -19,15 +22,9 @@ const activeItem = (currentTab, otherTab) => {
 };
 
 weekly.addEventListener("click", () => {
-	activeItem(
-		{ tab: weekly, form: weeklyForm },
-		{ tab: daily, form: dailyForm }
-	);
+	activeItem(WEEK_TAB, DAY_TAB);
 });
 
 daily.addEventListener("click", () => {
-	activeItem(
-		{ tab: daily, form: dailyForm },
-		{ tab: weekly, form: weeklyForm }
-	);
+	activeItem(DAY_TAB, WEEK_TAB);
 });
